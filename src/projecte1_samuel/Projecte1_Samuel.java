db81c010ef7905afc3e33a789c5c2549a60ba6e9
samuel18c <adam24scarrasco@inslaferreria.cat>
2025-05-21 20:16:28 +0200

    Hola afegit

---
+++ src/projecte1_samuel/Projecte1_Samuel.java
@@ -0,0 +1,13 @@
+
+package projecte1_samuel;
+
+
+public class Projecte1_Samuel {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola");
+    }
+    
+}
