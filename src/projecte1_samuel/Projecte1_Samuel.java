aa7f7a9f78f74666a54ed022a06407febaffa8e5
samuel18c <adam24scarrasco@inslaferreria.cat>
2025-05-22 17:34:30 +0200

    Jorgesout

--- src/projecte1_samuel/Projecte1_Samuel.java
+++ src/projecte1_samuel/Projecte1_Samuel.java
@@ -8,8 +8,8 @@ public class Projecte1_Samuel {
     public static void main(String[] args) {
         
         System.out.println("Hola");
-        
         System.out.println("Adeu");
+        System.out.println("Jorge");
     }
     
 }
