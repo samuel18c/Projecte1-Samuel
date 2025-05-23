1f45be9c4556de30f3c1c4efc7d4a3497b83742c
samuel18c <adam24scarrasco@inslaferreria.cat>
2025-05-23 16:06:35 +0200

    nuevo commit

---
+++ src/projecte1_samuel/Rama2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projecte1_samuel;
+
+/**
+ *
+ * @author samuc
+ */
+public class Rama2 {
+    
+}
